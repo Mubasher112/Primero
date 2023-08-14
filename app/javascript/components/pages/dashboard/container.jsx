1fffa1ca013e4a680cdd40280e5b9f7a5062c3a0
Mubasher Ayub <mubasher.rajput@kodesinc.com>
2023-08-14 19:13:38 +0500

    remove chart library that is no longer used

--- app/javascript/components/pages/dashboard/container.jsx
+++ app/javascript/components/pages/dashboard/container.jsx
@@ -1,8 +1,6 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Grid } from "@material-ui/core";
-import { Bar, Line, Doughnut, Pie } from "react-chartjs-2";
-import PropTypes from "prop-types";
 
 import { useI18n } from "../../i18n";
 import PageContainer, { PageHeading, PageContent } from "../../page";
@@ -31,73 +29,9 @@ import {
   WorkflowTeamCases
 } from "./components";
 import NAMESPACE from "./namespace";
+import { NAME } from "./constants";
 import { fetchDashboards, fetchFlags } from "./action-creators";
 
-// Custom PercentageTile component
-const PercentageTile = ({ label, percentage, count, color }) => {
-  return (
-    <div
-      style={{
-        border: "1px solid black",
-        padding: "10px",
-        textAlign: "center",
-        backgroundColor: color,
-        flex: "1",
-        margin: "0 5px"
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center"
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            marginBottom: "5px"
-          }}
-        >
-          <h2
-            style={{
-              marginRight: "5px",
-              fontSize: "30px",
-              fontWeight: "bold",
-              color: "white"
-            }}
-          >
-            {percentage}%
-          </h2>
-          <p
-            style={{
-              fontSize: "30px",
-              fontWeight: "bold",
-              color: "white",
-              margin: 0
-            }}
-          >
-            ({count})
-          </p>
-        </div>
-        <p style={{ margin: 0, color: "white", fontSize: "20px", fontWeight: "bold" }}>{label}</p>
-      </div>
-    </div>
-  );
-};
-
-PercentageTile.displayName = "PercentageTile";
-
-PercentageTile.propTypes = {
-  color: PropTypes.string.isRequired,
-  count: PropTypes.number.isRequired,
-  label: PropTypes.string.isRequired,
-  percentage: PropTypes.number.isRequired
-};
-
-// eslint-disable-next-line react/display-name
 const Dashboard = () => {
   const i18n = useI18n();
   const dispatch = useDispatch();
@@ -131,454 +65,37 @@ const Dashboard = () => {
     errors: flagsErrors
   };
 
-  // Chart data and options
-  // eslint-disable-next-line no-unused-vars
-  const [barChartData, setBarChartData] = useState({
-    labels: ["Quetta", "Khuzdar", "Chaman", "Gawadar", "Zhob"],
-    datasets: [
-      {
-        label: "No of Cases",
-        data: [10, 2, 3, 5, 1],
-        backgroundColor: [
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(54, 162, 235, 0.2)"
-        ],
-        borderColor: [
-          "rgba(75, 192, 192, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(54, 162, 235, 1)"
-        ],
-        borderWidth: 1
-      }
-    ]
-  });
-
-  // eslint-disable-next-line no-unused-vars
-  const [closedCasesByAgeChartData, setClosedCasesByAgeChartData] = useState({
-    labels: ["Age (Years)"],
-    datasets: [
-      {
-        label: "Exploitation",
-        data: [0],
-        backgroundColor: "LightCoral",
-        borderColor: "LightCoral",
-        borderWidth: 1
-      },
-      {
-        label: "Mental Violence",
-        data: [0],
-        backgroundColor: "Orange",
-        borderColor: "Orange",
-        borderWidth: 1
-      },
-      {
-        label: "Neglect And Negligent Treatment",
-        data: [0],
-        backgroundColor: "SeaGreen",
-        borderColor: "SeaGreen",
-        borderWidth: 1
-      },
-      {
-        label: "Physical Violence or Injury",
-        data: [0],
-        backgroundColor: "CornflowerBlue",
-        borderColor: "CornflowerBlue",
-        borderWidth: 1
-      },
-      {
-        label: "Sexual Abuse and Exploitation",
-        data: [0],
-        backgroundColor: "Purple",
-        borderColor: "Purple",
-        borderWidth: 1
-      }
-    ]
-  });
-
-  // eslint-disable-next-line no-unused-vars
-  const [closedCasesBySexChartData, setClosedCasesBySexChartData] = useState({
-    datasets: [
-      {
-        label: "Male",
-        data: [5],
-        backgroundColor: "CornflowerBlue",
-        borderColor: "CornflowerBlue",
-        borderWidth: 1
-      },
-      {
-        label: "Female",
-        data: [2],
-        backgroundColor: "LightCoral",
-        borderColor: "LightCoral",
-        borderWidth: 1
-      },
-      {
-        label: "Transgender",
-        data: [0],
-        backgroundColor: "Orange",
-        borderColor: "Orange",
-        borderWidth: 1
-      }
-    ],
-    labels: [
-      "Physical Violence or Injury",
-      "Mental Violence",
-      "Neglect And Negligent Treatment",
-      "Exploitation",
-      "Sexual Abuse and Exploitation"
-    ]
-  });
-
-  // eslint-disable-next-line no-unused-vars
-  const [casesAtGlanceData, setCasesAtGlanceData] = useState({
-    labels: ["Registerd (Open)", "Significant Harm (Total)", "Closed", "Assigned to Me"],
-    datasets: [
-      {
-        label: "Cases Status",
-        data: [300, 10, 0, 300],
-        backgroundColor: ["CornflowerBlue", "LightCoral", "Purple", "Orange"],
-        borderColor: ["CornflowerBlue", "LightCoral", "Purple", "Orange"],
-        borderWidth: 1
-      }
-    ]
-  });
-
-  const barChartOptions = {
-    responsive: true,
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  };
-
-  // eslint-disable-next-line no-unused-vars
-  const [lineChartData, setLineChartData] = useState({
-    labels: ["Oct", "Nov", "Dec", "Jan", "Feb", "Mar", "Apr", "May", "Jun"],
-    datasets: [
-      {
-        label: "Closed",
-        data: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        backgroundColor: "transparent",
-        borderColor: "CornflowerBlue",
-        borderWidth: 1
-      },
-      {
-        label: "Registered",
-        data: [0, 0, 0, 90, 0, 70, 0, 0, 0],
-        backgroundColor: "transparent",
-        borderColor: "Orange",
-        borderWidth: 1
-      }
-    ]
-  });
-
-  const lineChartOptions = {
-    responsive: true,
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  };
-
-  // eslint-disable-next-line no-unused-vars
-  const [significantHarmChartData, setSignificantHarmChartData] = useState({
-    labels: ["Physical", "Mental", "Neglect", "Exploitation", "Sexual Abuse"],
-    datasets: [
-      {
-        label: "Significant Harm Data",
-        data: [0, 60, 40, 0, 0],
-        backgroundColor: ["LightCoral", "CornflowerBlue", "Orange", "Purple", "Brown"],
-        borderColor: "white",
-        borderWidth: 1
-      }
-    ]
-  });
-
-  // eslint-disable-next-line no-unused-vars
-  const [casesWithCourtOrdersChartData, setCasesWithCoourtOrdersChartData] = useState({
-    labels: ["Supervision", "Custody and Replacement", "Interim", "Seek and Find"],
-    datasets: [
-      {
-        label: "Cases With Court OrdersData",
-        data: [0, 0, 100, 0, 0],
-        backgroundColor: ["LightCoral", "CornflowerBlue", "Orange", "Purple"],
-        borderColor: "white",
-        borderWidth: 1
-      }
-    ]
-  });
-
-  // eslint-disable-next-line no-unused-vars
-  const [casesRequiringChartData, setCasesRequiringChartData] = useState({
-    labels: ["Male", "Female", "Transgender"],
-    datasets: [
-      {
-        label: "Cases Requiring Alternatie Data",
-        data: [50, 50, 0],
-        backgroundColor: ["CornflowerBlue", "LightCoral", "Orange"],
-        borderColor: "white",
-        borderWidth: 1
-      }
-    ]
-  });
-
-  // eslint-disable-next-line no-unused-vars
-  const [doughnutChartData, setDoughnutChartData] = useState({
-    labels: ["Label 1", "Label 2", "Label 3", "Label 4", "Label 5"],
-    datasets: [
-      {
-        label: "Doughnut Data",
-        data: [10, 20, 30, 40, 50],
-        backgroundColor: ["LightCoral", "CornflowerBlue", "Purple", "Orange", "MediumSeaGreen"],
-        borderColor: "white",
-        borderWidth: 1
-      }
-    ]
-  });
-
-  const doughnutChartOptions = {
-    responsive: true
-  };
-
-  // eslint-disable-next-line no-unused-vars
-  const [casesRequiringSpecialConsiderationChartData, setCasesRequiringSpecialConsiderationChartDataChartData] =
-    useState({
-      labels: ["Minority Cases", "CwD Cases", "Cases with BISP Benif"],
-      datasets: [
-        {
-          label: "Pie Data",
-          data: [8, 80, 12],
-          backgroundColor: ["LightCoral", "CornflowerBlue", "Orange"],
-          borderColor: "white",
-          borderWidth: 1
-        }
-      ]
-    });
-
-  // eslint-disable-next-line no-unused-vars
-  const [staffBySexChartData, setStaffBySexChartData] = useState({
-    labels: ["Male", "Female", "Transgender"],
-    datasets: [
-      {
-        label: "Pie Data",
-        data: [100, 0, 0],
-        backgroundColor: ["LightCoral", "CornflowerBlue", "Orange"],
-        borderColor: "white",
-        borderWidth: 1
-      }
-    ]
-  });
-
-  const pieChartOptions = {
-    responsive: true
-  };
-
   return (
     <PageContainer>
       <PageHeading title={i18n.t("navigation.home")} />
       <PageContent>
         <OfflineAlert text={i18n.t("messages.dashboard_offline")} />
-        {/* Display PercentageTiles */}
-        <Grid container spacing={3}>
-          <Grid item xl={12} md={12} xs={12}>
-            <h4>Registered Cases Vs Services Provided</h4>
-          </Grid>
-          <Grid item xl={12} md={12} xs={12} style={{ display: "flex", justifyContent: "space-between" }}>
-            <PercentageTile label="Physical Violance or Injury" percentage={75} count={3} color="LightCoral" />
-            <PercentageTile label="Mental Violance" percentage={50} count={2} color="CornflowerBlue" />
-            <PercentageTile label="Neglect and Negligent Treatment" percentage={40} count={4} color="Purple" />
-            <PercentageTile label="Exploitation" percentage={90} count={5} color="Orange" />
-            <PercentageTile
-              label="Sexual Abuse and Sexual Exploitation"
-              percentage={60}
-              count={1}
-              color="MediumSeaGreen"
-            />
-          </Grid>
-        </Grid>
-        <Grid container spacing={3}>
-          <Grid item xl={6} md={6} xs={12}>
-            <Grid container spacing={3}>
-              <Grid item xl={12} md={12} xs={12}>
-                {/* Add the bar chart component with border */}
-                <h4>Cases at a Glance</h4>
-                <div style={{ border: "1px solid black" }}>
-                  <Bar data={casesAtGlanceData} options={barChartOptions} />
-                </div>
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item xl={6} md={6} xs={12}>
-            {/* Add the line chart component with border */}
-            <h4>Registered and Closed Cases by Month</h4>
-            <div style={{ border: "1px solid black" }}>
-              <Line data={lineChartData} options={lineChartOptions} />
-            </div>
-          </Grid>
-        </Grid>
-
-        <Grid container spacing={3}>
-          <Grid item xl={6} md={6} xs={12}>
-            <Grid container spacing={3}>
-              <Grid item xl={12} md={12} xs={12}>
-                {/* Add the doughnut chart component with border */}
-                <h4>Significant Harm Cases by Protection Concern</h4>
-                <div style={{ border: "1px solid black" }}>
-                  <Doughnut data={significantHarmChartData} options={doughnutChartOptions} />
-                </div>
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item xl={6} md={6} xs={12}>
-            {/* Add the bar chart component with border */}
-            <h4>Case Referrals (by Department)</h4>
-            <div style={{ border: "1px solid black" }}>
-              <Bar data={barChartData} options={barChartOptions} />
-            </div>
-          </Grid>
-        </Grid>
-
-        <Grid container spacing={3}>
-          <Grid item xl={6} md={6} xs={12}>
-            <Grid container spacing={3}>
-              <Grid item xl={12} md={12} xs={12}>
-                {/* Add the doughnut chart component with border */}
-                <h4>Cases Requiring Alternative Care Placement</h4>
-                <div style={{ border: "1px solid black" }}>
-                  <Doughnut data={casesRequiringChartData} options={doughnutChartOptions} />
-                </div>
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item xl={6} md={6} xs={12}>
-            {/* Add the doughnut chart component with border */}
-            <h4>Cases with Court Orders</h4>
-            <div style={{ border: "1px solid black" }}>
-              <Doughnut data={casesWithCourtOrdersChartData} options={doughnutChartOptions} />
-            </div>
-          </Grid>
-        </Grid>
-
-        <Grid container spacing={3}>
-          <Grid item xl={6} md={6} xs={12}>
-            <Grid container spacing={3}>
-              <Grid item xl={12} md={12} xs={12}>
-                {/* Add the doughnut chart component with border */}
-                <h4>Closed Cases by Sex and Potection Concern</h4>
-                <div style={{ border: "1px solid black" }}>
-                  <Bar data={closedCasesBySexChartData} options={barChartOptions} />
-                </div>
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item xl={6} md={6} xs={12}>
-            {/* Add the pie chart component with border */}
-            <h4>Cases Requiring Special Consideration</h4>
-            <div style={{ border: "1px solid black" }}>
-              <Pie data={casesRequiringSpecialConsiderationChartData} options={pieChartOptions} />
-            </div>
-          </Grid>
-        </Grid>
-
-        <Grid container spacing={3}>
-          <Grid item xl={6} md={6} xs={12}>
-            <Grid container spacing={3}>
-              <Grid item xl={12} md={12} xs={12}>
-                {/* Add the doughnut chart component with border */}
-                <h4>Staff By Sex</h4>
-                <div style={{ border: "1px solid black" }}>
-                  <Pie data={staffBySexChartData} options={pieChartOptions} />
-                </div>
-              </Grid>
-            </Grid>
-          </Grid>
-          <Grid item xl={6} md={6} xs={12}>
-            {/* Add the pie chart component with border */}
-            <h4>District wise social service workforce</h4>
-            <div style={{ border: "1px solid black" }}>
-              <Bar data={barChartData} options={barChartOptions} />
-            </div>
-          </Grid>
-        </Grid>
-
-        <Grid container spacing={3}>
-          <Grid item xl={12} md={12} xs={12}>
-            {/* Add the bar chart component with border */}
-            <h4>Closed Cases by Age and Protection Concern</h4>
-            <div style={{ border: "1px solid black" }}>
-              <Bar data={closedCasesByAgeChartData} options={barChartOptions} />
-            </div>
-          </Grid>
-        </Grid>
-
-        <Grid container spacing={3}>
-          <Grid item xl={12} md={12} xs={12}>
-            <h4>Percentage of Children who received Child Protection Services</h4>
-          </Grid>
-          <Grid item xl={12} md={12} xs={12} style={{ display: "flex", justifyContent: "space-between" }}>
-            <PercentageTile label="Physical Violance or Injury" percentage={75} count={3} color="LightCoral" />
-            <PercentageTile label="Mental Violance" percentage={50} count={2} color="CornflowerBlue" />
-            <PercentageTile label="Neglect and Negligent Treatment" percentage={40} count={4} color="Purple" />
-            <PercentageTile label="Exploitation" percentage={90} count={5} color="Orange" />
-            <PercentageTile
-              label="Sexual Abuse and Sexual Exploitation"
-              percentage={60}
-              count={1}
-              color="MediumSeaGreen"
-            />
-          </Grid>
-        </Grid>
-
         <Grid container spacing={3}>
           <Grid item xl={9} md={8} xs={12}>
             <Overview loadingIndicator={indicatorProps} userPermissions={userPermissions} />
             <WorkflowIndividualCases loadingIndicator={indicatorProps} />
             <CasesToAssign loadingIndicator={indicatorProps} />
+            <Approvals loadingIndicator={indicatorProps} />
             <SharedFromMyTeam loadingIndicator={indicatorProps} />
             <SharedWithMyTeam loadingIndicator={indicatorProps} />
-          </Grid>
-          <Grid item xl={3} md={4} xs={12}>
-            <Approvals loadingIndicator={indicatorProps} />
             <OverdueTasks loadingIndicator={indicatorProps} />
-            <Flags loadingIndicator={flagsIndicators} canFetchFlags={canFetchFlags} />
-          </Grid>
-        </Grid>
-
-        <Grid container spacing={3}>
-          <Grid item xl={6} md={6} xs={12}>
+            <CasesBySocialWorker loadingIndicator={indicatorProps} />
+            <WorkflowTeamCases loadingIndicator={indicatorProps} />
             <ReportingLocation loadingIndicator={indicatorProps} />
             <ProtectionConcern loadingIndicator={indicatorProps} />
-          </Grid>
-          <Grid item xl={6} md={6} xs={12}>
             <ViolationsCategoryVerificationStatus loadingIndicator={indicatorProps} />
             <ViolationsCategoryRegion loadingIndicator={indicatorProps} />
-          </Grid>
-        </Grid>
-
-        <Grid container spacing={3}>
-          <Grid item xl={6} md={6} xs={12}>
-            <CasesBySocialWorker loadingIndicator={indicatorProps} />
-          </Grid>
-          <Grid item xl={6} md={6} xs={12}>
             <PerpetratorArmedForceGroupPartyNames loadingIndicator={indicatorProps} />
           </Grid>
-        </Grid>
-
-        <Grid container spacing={3}>
-          <Grid item xl={6} md={6} xs={12}>
-            <WorkflowTeamCases loadingIndicator={indicatorProps} />
+          <Grid item xl={3} md={4} xs={12}>
+            <Flags loadingIndicator={flagsIndicators} />
           </Grid>
         </Grid>
       </PageContent>
     </PageContainer>
   );
 };
+
+Dashboard.displayName = NAME;
 
 export default Dashboard;

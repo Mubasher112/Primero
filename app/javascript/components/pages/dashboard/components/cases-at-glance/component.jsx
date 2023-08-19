7eed3998bd9dac43bd88493a60c93852f36edadb
Mubasher Ayub <mubasher.rajput@kodesinc.com>
2023-08-19 20:27:27 +0500

    Added Cases at glance stacked bar chart component and added dummy data that provided

--- app/javascript/components/pages/dashboard/components/cases-at-glance/component.jsx
+++ app/javascript/components/pages/dashboard/components/cases-at-glance/component.jsx
@@ -1,91 +1,80 @@
-import { useRef, useEffect } from "react";
-import Doughnut from "/../../app/javascript/components/dashboard/doughnut-chart";
-
 import { Grid } from "@material-ui/core";
 
+import { StackedBarChart } from "../../../../charts";
+
 const Component = () => {
-  const css = useStyles();
-  const dispatch = useDispatch();
-  const data = useMemoizedSelector(state => getRegisteredAndResolvedCasesByDistricts(state));
-  const stats = data.getIn(["data", "stats"]) ? data.getIn(["data", "stats"]).toJS() : null;
+  const chartData = {
+    labels: ["Registered", "Pakistani", "Other Nationality", "High", "Medium", "Low", "Closed cases", "Assigned to me"],
+    datasets: [
+      {
+        label: "Cases",
+        data: [93, 75, 18, 32, 28, 33, 85, 146],
+        backgroundColor: [
+          "Blue", // Registered
+          "Green", // Pakistani
+          "Orange", // Other Nationality
+          "#ED4E17", // High
+          "Yellow", // Medium
+          "#6EF6FE", // Low
+          "#A5A5A5", // Closed cases
+          "#A73621" // Assigned to me
+        ],
+        borderColor: [
+          "Blue",
+          "Green",
+          "Orange",
+          "#ED4E17",
+          "Yellow", // Black border for Medium for better visibility
+          "#6EF6FE",
+          "#A5A5A5",
+          "#A73621"
+        ],
+        borderWidth: 1
+      }
+    ]
+  };
 
-  useEffect(() => {
-    dispatch(fetchRegisteredAndResolvedCasesByDistricts());
-  }, []);
-
-  let graphData;
-  const chartOptions = {
+  const options = {
     scales: {
-      xAxes: [{
-        scaleLabel: {
-          display: true,
-          labelString: "Registered and Closed Cases",
-          fontColor: "red"
-        }
-      }],
-      yAxes: [{
-        scaleLabel: {
-          display: true,
-          labelString: "Number of Cases",
-          fontColor: "green"
-        }
-      }]
-    }
-  }
-
-  if (stats) {
-    const labels = [];
-    const reg = [];
-    const res = [];
-
-    for (const key in stats) {
-      labels.push(key);
-    }
-    for (const key in stats) {
-      reg.push(stats[key].registered_cases);
-    }
-    for (const key in stats) {
-      res.push(stats[key].resolved_cases);
-    }
-    graphData = {
-      labels,
-      datasets: [
+      xAxes: [
         {
-          label: "Closed",
-          data: res,
-          backgroundColor: "rgba(54, 162, 235, 0.5)",
-          borderColor: "rgb(54, 162, 235)",
-          borderWidth: 1,
-          stack: "Stack 0"
-        },
+          stacked: true,
+          scaleLabel: {
+            display: true,
+            labelString: "Cases Status"
+          }
+        }
+      ],
+      yAxes: [
         {
-          label: "Registered",
-          data: reg,
-          backgroundColor: "rgba(255, 159, 64, 0.5)",
-          borderColor: "rgb(255, 159, 64)",
-          borderWidth: 1,
-          stack: "Stack 0"
+          stacked: true,
+          ticks: {
+            beginAtZero: true,
+            stepSize: 20 // setting gap of 20
+          },
+          scaleLabel: {
+            display: true,
+            labelString: "No of cases"
+          }
         }
       ]
-    };
-  }
+    }
+  };
 
   return (
-    <>
-      {graphData && (
-        <Grid item md={6} xl={6}>
-          <div className={css.container}>
-            <h2>Registered and Closed Cases by District</h2>
-            <div className={css.card} flat>
-              <Doughnut options={chartOptions} data={graphData} showDetails />
-            </div>
+    <Grid item xl={6} md={6} xs={12}>
+      <Grid container spacing={3}>
+        <Grid item xl={12} md={12} xs={12}>
+          <h4>Cases at a Glance</h4>
+          <div style={{ border: "1px solid black" }}>
+            <StackedBarChart chartData={chartData} options={options} />
           </div>
         </Grid>
-      )}
-    </>
+      </Grid>
+    </Grid>
   );
 };
 
-Component.displayName = `HighRiskCases`;
+Component.displayName = `StackedBarChart`;
 
 export default Component;

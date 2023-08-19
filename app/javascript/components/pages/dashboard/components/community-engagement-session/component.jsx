8c3bf6d4fbf5ee412e6805d7f8e9cd68368131af
Mubasher Ayub <mubasher.rajput@kodesinc.com>
2023-08-19 20:36:43 +0500

    Added community engagement session doughnut chart in dashboard component

---
+++ app/javascript/components/pages/dashboard/components/community-engagement-session/component.jsx
@@ -0,0 +1,37 @@
+import { Grid } from "@material-ui/core";
+
+import { DoughnutChart } from "../../../../charts";
+
+const Component = () => {
+  const doughnutData = {
+    labels: ["Total Members", "Male", "Female", "Boys", "Girls"],
+    datasets: [
+      {
+        data: [50, 15, 13, 10, 12],
+        backgroundColor: ["blue", "green", "#FF1493", "#90EE90", "#FFB6C1"]
+      }
+    ]
+  };
+
+  const doughnutOptions = {
+    responsive: true,
+    legend: {
+      position: "top"
+    }
+  };
+
+  return (
+    <>
+      <Grid item xl={6} md={6} xs={12}>
+        <h4>Closed Cases by Sex and Protection Concern</h4>
+        <div style={{ border: "1px solid black" }}>
+          <DoughnutChart chartData={doughnutData} options={doughnutOptions} />
+        </div>
+      </Grid>
+    </>
+  );
+};
+
+Component.displayName = `DoughnutChart`;
+
+export default Component;

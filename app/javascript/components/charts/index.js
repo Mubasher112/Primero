8fe2ff8a2727edfa651e726d1efe1af7093adecc
Mubasher Ayub <mubasher.rajput@kodesinc.com>
2023-08-19 20:23:51 +0500

    Added new charts in index.js

--- app/javascript/components/charts/index.js
+++ app/javascript/components/charts/index.js
@@ -1,2 +1,5 @@
 export { default as BarChart } from "./bar-chart";
 export { default as TableValues } from "./table-values";
+export { default as StackedBarChart } from "./stacked-bar-chart";
+export { default as DoughnutChart } from "./doughnut-chart";
+export { default as LineChart } from "./line-chart";
